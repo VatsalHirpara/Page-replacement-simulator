06495ec6230f0db689569a4ac1eb1e7ec23326e5
Vatsal Hirpara <vatsalhirpara@yahoo.com>
2019-02-06 17:01:01 +0530

    Implement vmgen.java

--- src/vmgen.java
+++ src/vmgen.java
@@ -5,9 +5,21 @@ public class vmgen {
         int len=Integer.parseInt(args[1]);
         String input_file= args[2];
 
+        BufferedWriter wr=null;
         try{
-            FileWriter wr = new FileWriter("vmrefs.dat");
-            wr.write("5454445");
+             wr = new BufferedWriter(new FileWriter(input_file));
+             int temp1=0,temp2;
+             for(int i =1 ;i<=len;i++){
+                 temp2= (int) (Math.random()*range);
+                 if(i==1) temp1=temp2;
+
+                 while(temp1==temp2){
+                     temp2= (int) (Math.random()*range);
+                 }
+                 temp1=temp2;
+                 wr.write(String.valueOf(temp2));
+                 wr.write(" ");
+             }
             wr.close();
         }
         catch (IOException e) {
