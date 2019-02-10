a4ab8d2ce3c3b63cd500638877bb89d4957f1d61
Vatsal Hirpara <vatsalhirpara@yahoo.com>
2019-02-10 20:58:04 +0530

    implement vmstats

--- src/vmstats.java
+++ src/vmstats.java
@@ -1,5 +1,45 @@
+import java.io.*;
+
 public class vmstats {
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
+    public static void main(String[] args) throws IOException {
+        int min_frame = Integer.parseInt(args[0]);
+        int max_frame = Integer.parseInt(args[1]);
+        int increment = Integer.parseInt(args[2]);
+        String input_file = args[3];
+
+
+        PrintWriter writer = new PrintWriter("vmrates.dat");
+        writer.print("");
+        writer.close();
+        FileWriter fw = new FileWriter("vmrates.dat", true);
+        BufferedWriter bw = new BufferedWriter(fw);
+        for (int i = min_frame; i <=max_frame ;i+= increment ) {
+            bw.write(Integer.toString(i));
+            bw.write(" ");
+        }
+        bw.write("\n"); bw.flush();
+
+
+
+        for (int i = min_frame; i <=max_frame ;i+= increment ) {
+            System.out.print("opt, " + i + " frames :");
+            simulation_engine.run(i,input_file,"opt",false);
+        }
+        System.out.println("");
+        bw.write("\n"); bw.flush();
+
+        for (int i = min_frame; i <=max_frame ;i+= increment ) {
+            System.out.print("lru, " + i + " frames :");
+            simulation_engine.run(i,input_file,"lru",false);
+        }
+        System.out.println("");
+        bw.write("\n"); bw.flush();
+
+        for (int i = min_frame; i <=max_frame ;i+= increment ) {
+            System.out.print("fifo, " + i + " frames :");
+            simulation_engine.run(i,input_file,"fifo",false);
+        }
     }
+
+
 }
